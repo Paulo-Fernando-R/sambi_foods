61cec822b475a5ddff758020ece06e4080781f40
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-09-26 15:36:26 -0300

    "Added React Query to the project, wrapping Router with QueryClientProvider"

--- App.tsx
+++ App.tsx
@@ -1,4 +1,11 @@
 import Router from "./src/routes/Router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 export default function App() {
-    return <Router />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    );
 }
