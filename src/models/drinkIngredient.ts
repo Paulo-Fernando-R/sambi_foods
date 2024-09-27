1acd350b9bc902220c98bdeadd0b006eed2c7c64
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-09-27 17:57:04 -0300

    "Refactor DrinkIngredient class to use private fields with getters and setters"

--- src/models/drinkIngredient.ts
+++ src/models/drinkIngredient.ts
@@ -1,9 +1,25 @@
 export default class DrinkIngredient {
-    strIngredient1: string;
-    image: string;
+    private _strIngredient1: string;
+    private _image: string;
 
     constructor(ingredient: string) {
-        this.strIngredient1 = ingredient;
-        this.image = "www.thecocktaildb.com/images/ingredients/" + this.strIngredient1;
+        this._strIngredient1 = ingredient;
+        this._image = "www.thecocktaildb.com/images/ingredients/" + this.strIngredient1;
+    }
+
+    public get strIngredient1() {
+        return this._strIngredient1;
+    }
+
+    public get image() {
+        return this._image;
+    }
+
+    public set strIngredient1(ingridient: string) {
+        this._strIngredient1 = ingridient;
+    }
+
+    public set image(image: string) {
+        this._image = image;
     }
 }
